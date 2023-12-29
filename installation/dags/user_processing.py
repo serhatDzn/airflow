034003aa43892b3adfc58255ffbd425d37c22b84
serhatDzn <sdzn59@gmail.com>
2023-12-29 07:34:24 +0300

    first commit

---
+++ installation/dags/user_processing.py
@@ -0,0 +1,10 @@
+from airflow import DAG
+from datetime import datetime
+
+with DAG(
+    dag_id="user_processing",
+    start_date=datetime(2023, 1, 1),
+    schedule_interval="@daily",
+    catchup=False
+) as dag:
+    pass
